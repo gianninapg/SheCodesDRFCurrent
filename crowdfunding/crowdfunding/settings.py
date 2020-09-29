1f365f8726ad16f85230bb0567e6c4e47b22d021
gianninapg@gmail.com <Gi@090488>
2020-09-29 18:39:44 +1000

    update settings

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -54,7 +54,9 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
     'rest_framework.authentication.TokenAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
     ],
+
 }
 
 AUTH_USER_MODEL =    'users.CustomUser'
